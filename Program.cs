158e90b0ddecfda398c03a9181d0f7947749e75d
HOLYSHIELD <157357160+BLVCKDIAMONDSKY@users.noreply.github.com>
2025-04-10 13:30:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace starting_monkey_to_human_path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
