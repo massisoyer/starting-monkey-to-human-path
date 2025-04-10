0a463525f129026a523f08daecf62fa9bc89be11
massi <96039536+massisoyer@users.noreply.github.com>
2025-04-10 15:07:01 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I’m Danon Golygin, and I’m not a monkey");
         }
     }
 }
